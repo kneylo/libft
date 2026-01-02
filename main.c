c76eb23e8437a365511fdc88f7929f16bbb322bb
neylo <neylokreter@gmail.com>
2026-01-02 03:26:11 +0100

    no change, sandbox for functions

--- main.c
+++ main.c
@@ -2,15 +2,9 @@
     
 int main(void)
 {
-    char s1[] = "hello";
-    char s2[] = "world";
-    int value = nk_strcmp(s1, s2);
-   
-    if (value > 0)
-        printf("La première string est plus grande\n");
-    else if (value < 0)
-        printf("La deuxième string est plus grande\n");
-    else
-        printf("les 2 strings sont égales\n");
+    char str[] = "i want to capitalize this sentence";
+    nk_strcapitalize(str);
+    nk_putstr(str);
+    nk_putchar('\n');
     return(0);
 }
