c482df92914bf228cc7ef17f6146dd3738dfdc84
neylo <neylokreter@gmail.com>
2026-02-20 15:19:42 +0100

    correction itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -7,7 +7,7 @@ static size_t ft_len(long nb)
     len = 0;
     if (nb == -2147483648)
         return(11);
-    if (nb <= 0)
+    if (nb < 0)
         len++;
     while (nb != 0)
     {
@@ -20,31 +20,32 @@ static size_t ft_len(long nb)
 char    *ft_itoa(int n)
 {
     char *res;
-    long nbr;
     int len;
-    int i;
+    long nb;
 
-    nbr = n;
-    len = ft_len(nbr);
-    i = len;
+    nb = (long) nbr;
+    len = nblen(nb);
     res = malloc(sizeof(char) * (len + 1));
     if (!res)
-        return(NULL);
-    res[i--] = '\0';
-    if (nbr < 0)
+        return (NULL);
+    res[len--] = '\0';
+    if (nb == 0)
     {
-        nbr = -nbr;
+        res[0] = '0';
+        return res;
+    }
+    if (nb < 0)
+    {
+        nb *= -1;
         res[0] = '-';
     }
-    while (nbr != 0)
+    while (nb > 0)
     {
-        res[i] = (nbr % 10) + '0';
-        nbr = nbr / 10;
-        i--;
+        res[len] = (nb % 10) + '0';
+        nb /= 10;
+        len--;
     }
-    if (n == 0)
-        res[0] = '0';
-    return(res);
+    return (res);
 }
 
 /*int main(void)
