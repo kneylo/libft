10f79e90e08b4107985d3250f2239dc76f262bb7
neylo <neylokreter@gmail.com>
2026-03-25 00:13:20 +0100

    small correction

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -2,12 +2,7 @@
 
 int ft_isalpha(int c)
 {
-    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-    {
-        return(1);
-    }
-    else
-        return(0);
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 /*int main(void)
 {
--- ft_itoa.c
+++ ft_itoa.c
@@ -1,28 +1,24 @@
-#include "libft.h"
+#include <stdlib.h>
 
-int nblen(int nb)
+int nblen(int n)
 {
-    int len;
-    
-    len = 0;
-    if (nb == -2147483648)
-        return (11);
-    if (nb < 0)
+    int i;
+
+    i = 0;
+    if (n <= 0)
     {
-        nb *= -1;
-        len++;
+        i++;
+        n *= -1;
     }
-    if (nb == 0)
-        return (1);
-    while (nb > 0)
+    while (n != 0)
     {
-        nb /= 10;
-        len++;
+        i++;
+        n /= 10;
     }
-    return (len);
+    return (i);
 }
 
-char    *ft_itoa(int n)
+char *ft_itoa(int n)
 {
     char *res;
     int len;
@@ -34,14 +30,9 @@ char    *ft_itoa(int n)
     if (!res)
         return (NULL);
     res[len--] = '\0';
-    if (nb == -2147483648)
-    {
-        res = '-2147483648';
-        return (res);
-    }
     if (nb == 0)
     {
-        res[0] = '0';
+        res[len] = '0';
         return (res);
     }
     if (nb < 0)
@@ -49,18 +40,20 @@ char    *ft_itoa(int n)
         nb *= -1;
         res[0] = '-';
     }
-    while (nb > 0)
+    while (nb)
     {
-        res[len] = (nb % 10) + '0';
+        res[len--] = (nb % 10) + '0';
         nb /= 10;
-        len--;
     }
     return (res);
 }
 
 /*int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", ft_itoa(ft_atoi(argv[1])));
-    return (0);
+    if (argc == 2)
+    {
+        printf("%s\n", ft_itoa(atoi(argv[1])));
+        return (0);
+    }
+    return (1);
 }*/
