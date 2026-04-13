43ed54f4f08bc08b43588f5f767b1e6d581bfcee
neylo <neylokreter@gmail.com>
2026-04-13 13:26:48 +0200

    small correction on both strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -8,13 +8,9 @@ int ft_strcmp(char *s1, char *s2)
     while(s1[i] || s2[i])
     {
         if (s1[i] != s2[i])
-        {
             return(s1[i] - s2[i]);
-        }
         else
-        {
             i++;
-        }
     }
     return(0);
 }
