9489d251ef14d224962a1ced36f24d80a7306c8c
neylo <neylokreter@gmail.com>
2026-07-16 17:57:35 +0200

    strdup minus changes

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,16 @@
 #include "libft.h"
 
-char       *ft_strdup(const char *s)
+char    *ft_strdup(const char *s)
 {
     char *res;
-    size_t i;
 
     if (!s)
         return (NULL);
     res = malloc(sizeof(char) * (ft_strlen(s) + 1));
     if(!res)
-        return(NULL);
-    i = 0;
-    while(s[i])
-    {
-        res[i] = s[i];
-        i++;
-    }
-    res[i] = '\0';
-    return(res);
+        return (NULL);
+    ft_memcpy(res, s, ft_strlen(s) + 1)
+    return (res);
 }
 
 /*int main(int argc, char *argv[])
